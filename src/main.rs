d38e15b6ac02cda68de8c5706f66be990c269351
Puskar Roy <113108193+Puskar-Roy@users.noreply.github.com>
2024-12-05 23:50:17 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,6 @@ async fn main() -> std::io::Result<()> {
 
     if std::env::var_os("RUST_LOG").is_none(){
         std::env::set_var("RUST_LOG", "actix_web=info");
-
     }
     dotenv::dotenv().ok();
     env_logger::init();
